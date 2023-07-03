cc14cd7e0427792865930b536bb3f1cfb52af38d
ｒｙｍｅ <ryme.care@hotmail.com>
2023-07-03 13:45:25 +0530

    updates

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "pimon")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tg_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '1001956882347'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'iPrimeHub')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
