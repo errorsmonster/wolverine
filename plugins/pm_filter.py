afd7d68c0a5435d286a8afaf2954ae60d4073d0d
ｒｙｍｅ <ryme.care@hotmail.com>
2023-07-03 18:22:51 +0530

    updates

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -29,7 +29,7 @@ SPELL_CHECK = {}
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def private_filter(client, message):
-    await auto_filter(client, message)
+    await paid_filter(client, message)
 
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
