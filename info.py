3f4390918f91d4ac76e0028a6ed1b8bc5d832ad5
ｒｙｍｅ <ryme.care@hotmail.com>
2023-07-03 13:40:54 +0530

    updats

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://rahul:rahul@rahul.8qehd6b.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "pimon")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'tg_files')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
