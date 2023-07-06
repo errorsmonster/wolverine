e99357aad904e54f229c45c1316d01ffe43e3957
ｒｙｍｅ <ryme.care@hotmail.com>
2023-07-06 13:04:03 +0530

    updated

--- plugins/private_filters.py
+++ plugins/private_filters.py
@@ -32,7 +32,7 @@ async def private_filter(client, message):
         
         
 @Client.on_callback_query(filters.regex(r"^pnext"))
-async def next_page(bot, query):
+async def pnext_page(bot, query):
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
         return await query.answer("oKda", show_alert=True)
