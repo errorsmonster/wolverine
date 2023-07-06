4bc7e4c0c89080d981c588594696e192c05896ef
ｒｙｍｅ <ryme.care@hotmail.com>
2023-07-06 10:05:55 +0530

    updated

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -746,7 +746,7 @@ async def auto_filter(client, msg, api=None, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {await replace_blacklist(file.file_name, blacklist)}", 
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
                     url=await short_links(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}", api)
                 ),
             ]
