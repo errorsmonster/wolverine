81feecccabc2cf44c4fca88309d75e78384d3197
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-07 22:36:42 +0530

    updated

--- plugins/auto_approve.py
+++ plugins/auto_approve.py
@@ -6,12 +6,15 @@ TEXT = "Hello {}, Welcome To {}"
 APPROVED = True  # set to True by default
 
 # Define the decorator function to handle chat join requests
-@Client.on_chat_join_request(filters.group | filters.channel & filters.chat(CHAT_IDS))
-async def autoapprove(client: Client, request: ChatJoinRequest):
-    chat = request.chat
-    user = request.from_user
-    print(f"{user.first_name} joined {chat.title}")
-
-    if APPROVED:
-        await request.approve()
-        await client.send_message(chat.id, TEXT.format(user.mention(), chat.title))
+@Client.on_chat_join_request(filters.chat(CHAT_IDS))
+def auto_approve(client: Client, message: ChatJoinRequest):
+    while APPROVED:
+        try:
+            client.approve_chat_join_request(message.chat.id, message.user.id)
+            client.send_message(
+                message.chat.id,
+                TEXT.format(message.from_user.first_name, message.chat.title),
+            )
+        except Exception as e:
+            print(e)
+            break
