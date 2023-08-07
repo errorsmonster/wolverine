51dfda69004b14289810c7c9e894f9fc32cc87d0
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-07 22:55:21 +0530

    updated

--- bot.py
+++ bot.py
@@ -1,7 +1,17 @@
+import logging
+import logging.config
+
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
+
 from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
@@ -34,11 +44,16 @@ class Bot(Client):
         temp.BANNED_CHATS = b_chats
         await super().start()
         await Media.ensure_indexes()
-        print(name)
+        me = await self.get_me()
+        temp.ME = me.id
+        temp.U_NAME = me.username
+        temp.B_NAME = me.first_name
+        self.username = '@' + me.username
+        logging.info(name)
 
     async def stop(self, *args):
         await super().stop()
-        print("Bot stopped. Bye.")
+        logging.info("Bot stopped. Bye.")
     
     async def iter_messages(
         self,
