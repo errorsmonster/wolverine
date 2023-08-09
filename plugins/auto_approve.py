89b0009f5edf4ac639d197f74599c0a0d8d583bc
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-09 11:09:28 +0530

    updated

--- plugins/auto_approve.py
+++ plugins/auto_approve.py
@@ -1,20 +1,21 @@
 from pyrogram import Client, filters
 from info import AUTH_GROUPS as CHAT_IDS
+from pyrogram.errors import FloodWait
 from pyrogram.types import ChatJoinRequest
 
 TEXT = "Hello {}, Welcome To {}"
 APPROVED = True  # set to True by default
 
-# Define the decorator function to handle chat join requests
 @Client.on_chat_join_request(filters.chat(CHAT_IDS))
 def auto_approve(client: Client, message: ChatJoinRequest):
-    while APPROVED:
+    if APPROVED:
         try:
             client.approve_chat_join_request(message.chat.id, message.user.id)
             client.send_message(
                 message.chat.id,
                 TEXT.format(message.from_user.first_name, message.chat.title),
             )
+        except FloodWait as e:
+            print(f"Rate limit exceeded. Retry after {e.x} seconds.")
         except Exception as e:
-            print(e)
-            break
+            print("An error occurred:", e)
