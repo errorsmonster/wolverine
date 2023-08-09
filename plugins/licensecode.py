13f9c34c309b19c99e73e4fcbe2b31aa16c39d4e
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-09 23:04:57 +0530

    Update licensecode.py

--- plugins/licensecode.py
+++ plugins/licensecode.py
@@ -37,7 +37,7 @@ async def generate(client, message):
     await message.reply_text(f"<b>Generated redeem codes:</b>\n<code>{codes_str}</code>")
 
 
-@Client.on_message(filters.regex(r"^[A-Z0-9]{10}$") & filters.private)
+@Client.on_message(filters.regex(r"^[A-Z0-9]{20}$") & filters.private)
 async def validate_code(client, message):
     code = message.text
     user_id = message.from_user.id
