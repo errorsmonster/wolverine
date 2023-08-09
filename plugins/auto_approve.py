3284ebff546be31ae11ea0079e38c6ef155aaa8d
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-09 19:45:43 +0530

    updated

--- plugins/auto_approve.py
+++ plugins/auto_approve.py
@@ -1,16 +1,16 @@
 from pyrogram import Client, filters
-from info import AUTH_GROUPS as CHAT_IDS
+from info import AUTH_GROUPS
 from pyrogram.errors import FloodWait
 from pyrogram.types import ChatJoinRequest
 
 TEXT = "Hello {}, Welcome To {}"
 APPROVED = True  # set to True by default
 
-@Client.on_chat_join_request(filters.chat(CHAT_IDS))
+@Client.on_chat_join_request(filters.chat(AUTH_GROUPS))
 def auto_approve(client: Client, message: ChatJoinRequest):
     if APPROVED:
         try:
-            client.approve_chat_join_request(CHAT_IDS, message.user.id)
+            client.approve_chat_join_request(AUTH_GROUPS, message.user.id)
             client.send_message(
                 message.chat.id,
                 TEXT.format(message.from_user.first_name, message.chat.title),
