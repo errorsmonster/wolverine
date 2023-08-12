cf7c977444499e8a2946ee190c0d1d2f9e8d57e7
ｒｙｍｅ <ryme.care@hotmail.com>
2023-08-12 12:16:50 +0530

    updated

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -4,14 +4,14 @@ from info import FORCESUB_CHANNEL
 from database.users_chats_db import db
 
 
-@Client.on_chat_join_request(filters.channel & filters.chat(FORCESUB_CHANNEL))
+@Client.on_chat_join_request(filters.chat(FORCESUB_CHANNEL))
 async def private_fsub(client: Client, message: ChatJoinRequest):
     user = message.from_user.id
+    print(f"{user.first_name} Added to database")
+    if await db.is_user_joined(user):
+        return
     try:
-        if await db.is_user_joined(user):
-            return
-        else:
-            await db.update_user_joined(user)
+        await db.update_user_joined(user)
     except Exception as e:
         print(e)
         pass
