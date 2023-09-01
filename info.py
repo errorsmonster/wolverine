999017f65c4c937f1271e962af3781834d90ef44
ｒｙｍｅ <ryme.care@hotmail.com>
2023-09-01 12:58:20 +0530

    update

--- info.py
+++ info.py
@@ -31,8 +31,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://reen:reen@reen.9aocxh7.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "PrimeHub")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ryu:ryu@ryu.watcr8f.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "primehub")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'ryu')
 
 # Others
