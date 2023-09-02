0dae71aea61cd012f8766f321985c5255e2385f0
ｒｙｍｅ <ryme.care@hotmail.com>
2023-09-02 13:29:55 +0530

    updated

--- Script.py
+++ Script.py
@@ -46,7 +46,7 @@ Name - {}
 """
 
     #paid group
-    GROUP_PROMO = """Now You Can **Earn Money** By Adding This Bot In Your Group.\n\n**Requirements:**\n1. Your Group Must Have Minimum 500 Members.\n2. Your Group Must Be Active.\n3. Your Group Must Be Public. \n\n If You Are Interested Then Contact <a href=https://t.me/lemx4>ｒｙｍｅ</a>"""
+    GROUP_PROMO = """Now You Can **Earn Money** By Adding This Bot In Your Group.\n\n**Requirements:**\n1. Your Group Must Have Minimum 500 Members.\n2. Your Group Must Be Active.\n3. Your Group Must Be Public.\n\n**Advantages:**\n1. No need To Manage The Bot\n2. No Need to Upload Movies\n3. Use Your Favourite Shortner \n\n If You Are Interested Then Contact <a href=https://t.me/lemx4>ｒｙｍｅ</a>"""
 
     # removing blacklisted words
     BLACKLIST = ['www', 'tamilblaster', 'filmyzilla', 'streamershub', 'xyz', 'cine', 'link',
