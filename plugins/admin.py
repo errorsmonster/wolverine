f27e208f602e10150428052e1ce86b40a9396346
ｒｙｍｅ <ryme.care@hotmail.com>
2023-09-02 09:35:43 +0530

    update

--- plugins/admin.py
+++ plugins/admin.py
@@ -145,7 +145,6 @@ async def userinfo(client, message):
     user = await client.get_users(user_id)
     name = user.first_name if not user.last_name else f"{user.first_name} {user.last_name}"
     link = f"<a href='tg://user?id={user_id}'>{name}</a>"
-    total_files_sent = await db.get_lifetime_files(user_id)
     total_files_sent_today = await db.get_files_count(user_id)
     private_joined = await db.is_user_joined(user_id)
 
@@ -171,6 +170,5 @@ async def userinfo(client, message):
         f"<b>Expiry Date:</b> <code>{expiry_date}</code>\n"
         f"<b>Days Left:</b> <code>{days_left}</code>\n"
         f"<b>Files Sent Today:</b> <code>{total_files_sent_today}</code>\n"    
-        f"<b>Total Files Sent:</b> <code>{total_files_sent}</code>\n",
         disable_web_page_preview=True
     )
