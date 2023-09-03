a5bcc4acd77f7cfed4a8868b6efd96029c015912
ｒｙｍｅ <ryme.care@hotmail.com>
2023-09-04 00:00:42 +0530

    updated

--- plugins/admin.py
+++ plugins/admin.py
@@ -162,11 +162,12 @@ async def userinfo(client, message):
         purchase_date_unix = users.get("purchase_date")
         status = "Premium"
         purchase_date = datetime.fromtimestamp(purchase_date_unix).strftime("%d/%m/%Y")
-        expiry_date = (purchase_date + timedelta(days=duration)).strftime("%d/%m/%Y")
         days_left = (datetime.fromtimestamp(purchase_date_unix) - datetime.now()).days
+        expiry_date = purchase_date + timedelta(days=duration)
+        purchase_date_str = purchase_date.strftime("%d/%m/%Y")
     else:
         status = "Free"
-        purchase_date = "N/A"
+        purchase_date_str = "N/A"
         expiry_date = "N/A"
         days_left = "N/A"
         duration = "N/A"
@@ -178,7 +179,7 @@ async def userinfo(client, message):
         f"<b>➲DC ID:</b> {dc_id}\n"
         f"<b>➲Subscribed:</b> {private_joined}\n"
         f"<b>➲Status:</b> {status}\n"
-        f"<b>➲Purchase Date:</b> <code>{purchase_date}</code>\n"
+        f"<b>➲Purchase Date:</b> <code>{purchase_date_str}</code>\n"
         f"<b>➲Expiry Date:</b> <code>{expiry_date}</code>\n"
         f"<b>➲Days Left:</b> <code>{days_left}/{duration}</code> days\n"
         f"<b>➲Files Recieved:</b> <code>{total_files_sent}</code>\n"
