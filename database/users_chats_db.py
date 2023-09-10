ea015d389b6306b4d57602240059d3231e01fd12
ｒｙｍｅ <ryme.care@hotmail.com>
2023-09-10 08:56:45 +0530

    updated

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -166,32 +166,6 @@ class Database:
             ),
         )
          
-    async def update_api_for_group(self, group_id, api):
-        await self.grp.update_one({'id': int(group_id)}, {'$set': {'api': api}})
-        
-    async def remove_api_for_group(self, group_id):
-        await self.grp.update_one({'id': group_id}, {'$set': {'api': 0}})
-
-    async def update_shortner_and_api(self, group_id, shortner, api):
-        await self.grp.update_one({'id': int(group_id)}, {'$set': {'shortner': shortner, 'api': api}})
-
-    async def get_shortner_from_chat(self, group_id):
-        group = await self.grp.find_one({'id': int(group_id)})
-        shortner = group.get('shortner', '')
-        if not shortner or shortner == 0:
-            return False
-        return shortner
-    
-    async def rempove_shortner_for_group(self, group_id):
-        await self.grp.update_one({'id': int(group_id)}, {'$set': {'shortner': None, 'api': 0}})
-
-    async def get_api_from_chat(self, group_id):
-        group = await self.grp.find_one({'id': int(group_id)})
-        api = group.get('api', '')
-        if not api or api == 0:
-            return False
-        return api
-
     async def add_user(self, id, name):
         user = self.new_user(id, name)
         await self.col.insert_one(user)
