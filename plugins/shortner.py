11952a11f88c10de2e1a2292e1dac131ac2563ed
ｒｙｍｅ <ryme.care@hotmail.com>
2023-09-27 16:26:14 +0530

    Update shortner.py

--- plugins/shortner.py
+++ plugins/shortner.py
@@ -1,6 +1,7 @@
 import aiohttp
+import json
 
-async def get_shortlink(link):
+async def linkgen(link):
     url = f'https://api.shareus.io/easy_api'
     api_key = "VLuKAPjHgrahNY2zTcWM16lFyTJ2"
 
@@ -25,3 +26,15 @@ async def adlinkfly(link, shortner=None, api_key=None):
     except Exception as e:
         print(e)
         return f"{shortner}?api={api_key}&url={link}&format=text"
+
+async def get_shortlink(link):
+    shortner = f'https://urlshare.onrender.com/?create&url={link}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(shortner, raise_for_status=True) as response:
+                data = await response.json()
+                return data["short_url"]
+    except Exception as e:
+        print(e)
+
+
