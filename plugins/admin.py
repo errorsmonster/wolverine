5f7e88422c1a66f7511ff2bc6b1b2b47cf4260a9
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-01 13:49:45 +0530

    updated

--- plugins/admin.py
+++ plugins/admin.py
@@ -268,7 +268,7 @@ async def allcommands(client, message):
 async def reffer(client, message):
     m = await message.reply_text('Creating your refferal link...')
     user_id = message.from_user.id
-    refferal_link = f"https://t.me/{temp.U_NAME}?start=ref#{user_id}"
+    refferal_link = f"https://t.me/{temp.U_NAME}?start=ref_{user_id}"
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("🔗 Invite Your Friends", url=f"https://telegram.me/share/url?url={refferal_link}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83")]])
     await m.edit(f"Your refferal link is - \n{refferal_link}\n\nShare this link with your friends, each time they join, you will get 10 refferal points and after 100 points you will get 1 month premium subscription.",
                  reply_markup=keyboard,
