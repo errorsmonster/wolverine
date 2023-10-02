e6d0aad971440634db1a302f185818ae0b1b8bb6
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-02 19:39:46 +0530

    updated

--- plugins/commands.py
+++ plugins/commands.py
@@ -36,8 +36,6 @@ async def start(client, message):
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
-    if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id, message.from_user.first_name)
     if len(message.command) != 2:
         buttons = [[
                     InlineKeyboardButton('💡 How To Download', url=f"https://t.me/QuickAnnounce/5")
