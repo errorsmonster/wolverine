634b8edca50d054903c9d36f8851fc4c6d022841
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-03 10:54:14 +0530

    updated

--- plugins/commands.py
+++ plugins/commands.py
@@ -53,7 +53,7 @@ async def start(client, message):
         )
         return
     data = message.command[1]
-    if not data.split("-", 1)[0] == "RefferID" and FORCESUB_CHANNEL and not await is_subscribed(client, message):
+    if not data.split("-", 1)[0] == "ReferID" and FORCESUB_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(FORCESUB_CHANNEL), creates_join_request=True)
         except Exception as e:
