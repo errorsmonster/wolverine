9bb34a116988059cd96986195176302a601c4e2a
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-03 09:54:41 +0530

    updated

--- plugins/admin.py
+++ plugins/admin.py
@@ -274,12 +274,13 @@ async def allcommands(client, message):
         )
     
 # Add functions for refferal system
-@Client.on_message(filters.command("reffer") & filters.private)
+@Client.on_message(filters.command("refer") & filters.private)
 async def reffer(client, message):
-    m = await message.reply_text('Creating your refferal link...')
+    m = await message.reply_text('Generating your refferal link...')
+    await asyncio.sleep(2)
     user_id = message.from_user.id
-    refferal_link = f"https://t.me/{temp.U_NAME}?start=RefferID-{user_id}"
+    refferal_link = f"https://t.me/{temp.U_NAME}?start=ReferID-{user_id}"
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("🔗 Invite Your Friends", url=f"https://telegram.me/share/url?url={refferal_link}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83")]])
-    await m.edit(f"<b>Here is your refferal link:</b>\n{refferal_link}\n\nShare this link with your friends, each time they join, you will get 10 refferal points and after 100 points you will get 1 month premium subscription.",
+    await m.edit(f"<b>Here is your refferal link:</b>\n{refferal_link}\n\n<b>Share this link with your friends, each time they join, you will be rewarded 10 refferal points and after 100 points you will get 1 month premium subscription.</b>",
                  reply_markup=keyboard,
                  disable_web_page_preview=True)
--- plugins/commands.py
+++ plugins/commands.py
@@ -192,7 +192,7 @@ async def start(client, message):
         return await sts.delete()
         
 
-    elif data.split("-", 1)[0] == "RefferID":
+    elif data.split("-", 1)[0] == "ReferID":
         invite_id = data.split("-", 1)[1]
         user_id = message.from_user.id
         invited_user = await client.get_users(invite_id)
@@ -203,7 +203,7 @@ async def start(client, message):
         if not await db.is_user_exist(user_id):
             refferal = await db.get_refferal_count(invite_id)
             await db.add_user(user_id, message.from_user.first_name)
-            await db.update_refferal_count(invite_id, refferal + 1)
+            await db.update_refferal_count(invite_id, refferal + 10)
             await client.send_message(text=f"You have successfully Invited {message.from_user.mention}", chat_id=invite_id)
             await message.reply_text(f"You successfully Invited by {invusername}", disable_web_page_preview=True)
         else:
