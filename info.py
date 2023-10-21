2a563605c6a9213fb5a2835d8326a3c80722d014
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-21 14:46:24 +0530

    updated

--- info.py
+++ info.py
@@ -52,10 +52,16 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
+# forcesub cahnnel or group
 FORCESUB_CHANNEL = int(environ.get('FORCESUB_CHANNEL', "-1001905287436"))
+# flood wait timer
 SLOW_MODE_DELAY = int(environ.get('SLOW_MODE_DELAY', 60))
+# daily request limit for individual user
 RESPONCE_LIMIT = int(environ.get('RESPONCE_LIMIT', 10))
+# for giving free link {without ads}
 ONE_LINK_ONE_FILE = is_enabled((environ.get('ONE_LINK_ONE_FILE', "True")), False)
+# same as auth group
 accss_grp = environ.get('ACCESS_GROUP', "-1001786962803")
 ACCESS_GROUPS = [int(ch) for ch in accss_grp.split()] if auth_grp else None
+# wait time for automate delete qurery request and query feed
 WAIT_TIME = int(environ.get('AUTO_DELETE_WAIT_TIME', 600))
