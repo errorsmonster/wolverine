e9dd2b4d0f34c1722ba88682f9fde335498d7081
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-23 15:11:03 +0530

    updated

--- plugins/approve.py
+++ plugins/approve.py
@@ -1,16 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import ChatJoinRequest
-from info import AUTH_GROUPS
+from info import AUTH_GROUPS, APPROVE
 
 
 TEXT = "Hello {}, Welcome To {}"
-APPROVE = True
 
 # auto approve members 
-@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else (filters.group | filters.channel))
+@Client.on_chat_join_request(filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.group)
 async def autoapprove(client: Client, message: ChatJoinRequest):
     chat=message.chat
     user=message.from_user
-    if APPROVE == True:
+    if APPROVE:
         await client.approve_chat_join_request(chat.id, user.id)
         await client.send_message(chat_id=chat.id, text=TEXT.format(user.mention, chat.title))
