f0875396565b6b3655bda7292160e7bcd8df2c8d
ｒｙｍｅ <ryme.care@hotmail.com>
2023-10-23 14:57:10 +0530

    updated

--- plugins/admin.py
+++ plugins/admin.py
@@ -282,12 +282,12 @@ async def allcommands(client, message):
 # Add functions for refferal system
 @Client.on_message(filters.command("refer") & filters.private)
 async def reffer(client, message):
-    m = await message.reply_text('Generating your refferal link...')
+    m = await message.reply_text(f"<b>Generating Your Refferal Link...</b>")
     await asyncio.sleep(2)
     user_id = message.from_user.id
     referral_points = await db.get_refferal_count(user_id)
     refferal_link = f"https://t.me/{temp.U_NAME}?start=ReferID-{user_id}"
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("🔗 Invite Your Friends", url=f"https://telegram.me/share/url?url={refferal_link}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83")]])
-    await m.edit(f"<b>Here is your refferal link:</b>\n{refferal_link}\n\n<b>Share this link with your friends, each time they join, Both of you will be rewarded 10 refferal points and after 100 points you will get 1 month premium subscription.\n\n Referral Points: {referral_points}</b>",
+    await m.edit(f"<b>Here is your refferal link:\n\n{refferal_link}\n\nShare this link with your friends, Each time they join, Both of you will be rewarded 10 refferal points and after 100 points you will get 1 month premium subscription.\n\n Referral Points: {referral_points}</b>",
                  reply_markup=keyboard,
                  disable_web_page_preview=True)
