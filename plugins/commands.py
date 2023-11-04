042b54c92958d2cc8c211e20921b848221307eec
ｒｙｍｅ <ryme.care@hotmail.com>
2023-11-04 22:44:37 +0530

    updated

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,7 +43,8 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('💡 How To Download', url=f"https://t.me/QuickAnnounce/5")
                     ],[
-                    InlineKeyboardButton('📎 Refer & Get Premium', callback_data="refer"),
+                    InlineKeyboardButton('📎 Refer', callback_data="refer"),
+                    InlineKeyboardButton('🔥 Top Search', url=f"https://t.me/{temp.U_NAME}?start=topsearch"),
                     ],[
                     InlineKeyboardButton('🎟️ Upgrade ', callback_data="remads"),
                     InlineKeyboardButton('🗣️ Request', callback_data="request")
