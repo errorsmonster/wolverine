af5d4a5d310d5fbfd7cd6d39bfb83f2375ff112c
L E M O N <ryme.care@hotmail.com>
2023-11-23 23:22:30 +0530

    updated

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -158,7 +158,6 @@ async def filters_private_handlers(client, message):
                 m = await msg.edit(text=auto, reply_markup=keyboard, disable_web_page_preview=True)
  
     except Exception as e:
-        print(e)
         await msg.edit(f"<b>Opps! Something Went Wrong.</b>")
 
     finally:
@@ -306,24 +305,6 @@ async def advantage_spoll_choker(bot, query):
             k = await query.message.edit('This Movie Not Found In DataBase')
             await asyncio.sleep(10)
             await k.delete()
-
-async def delete_files(query, limit, file_type):
-    k = await query.message.edit(text=f"Deleting <b>{file_type.upper()}</b> files...", reply_markup=None)
-    files, _, _ = await get_search_results(file_type.lower(), max_results=limit, offset=0)
-    deleted = 0
-
-    for file in files:
-        file_ids = file.file_id
-        result = await Media.collection.delete_one({'_id': file_ids})
-
-        if result.deleted_count:
-            logger.info(f'{file_type.capitalize()} File Found! Successfully deleted from database.')
-
-        deleted += 1
-
-    deleted = str(deleted)
-    await k.edit_text(text=f"<b>Successfully deleted {deleted} {file_type.upper()} files.</b>")
-
     
 
 @Client.on_callback_query()
@@ -662,37 +643,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         reply_markup=InlineKeyboardMarkup(buttons),
         disable_web_page_preview=True,
         )
-
-    elif query.data == "refer":
-        user_id = query.from_user.id
-        referral_points = await db.get_refferal_count(user_id)
-        refferal_link = f"https://t.me/{temp.U_NAME}?start=ReferID-{user_id}"
-        buttons = [[
-                    InlineKeyboardButton('🎐 Invite', url=f"https://telegram.me/share/url?url={refferal_link}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83"),
-                    InlineKeyboardButton(f"🟢 {referral_points}", callback_data="refer_point"),
-                    InlineKeyboardButton('◀️ Back', callback_data="home")
-                ]]
-        await query.message.edit(
-            text=f"<b>Here is your refferal link:\n\n{refferal_link}\n\nShare this link with your friends, Each time they join, Both of you will get 10 refferal points and after 50 points you will get 1 month premium subscription.</b>",
-            reply_markup=InlineKeyboardMarkup(buttons),
-            disable_web_page_preview=True,
-        )
-
-    elif query.data == "refer_point":
-        user_id = query.from_user.id
-        referral_points = await db.get_refferal_count(user_id)
-        await query.answer(f"You have {referral_points} refferal points.", show_alert=True
-        )
-
-    elif query.data == "terms":
-        buttons = [[
-                    InlineKeyboardButton("✅ Accept Terms", callback_data="home"),
-                ]]
-        await query.message.edit(
-            text=script.TERMS,
-            reply_markup=InlineKeyboardMarkup(buttons),
-            disable_web_page_preview=True,
-        )
                 
     elif query.data.startswith("setgs"):
         ident, set_type, status, grp_id = query.data.split("#")
@@ -726,143 +676,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             ]
             reply_markup = InlineKeyboardMarkup(buttons)
             await query.message.edit_reply_markup(reply_markup)
-
-    # Function to delete unwanted files
-    elif query.data == "delback":
-        keyboard_buttons = [
-            ["PreDVD", "PreDVDRip"],
-            ["HDTS", "HDTC"],
-            ["HDCam", "Sample"],
-            ["CamRip", "Print"]
-        ]
-
-        btn = [
-            [InlineKeyboardButton(button, callback_data=button.lower().replace("-", "")) for button in row]
-            for row in keyboard_buttons
-        ]
-        btn.append(
-            [InlineKeyboardButton("⛔️ Close", callback_data="close_data")]
-            )
-
-        await query.message.edit(
-            text="<b>Select The Type Of Files You Want To Delete..?</b>",
-            reply_markup=InlineKeyboardMarkup(btn)
-        )
-        
-    elif query.data in ["predvd", "camrip", "predvdrip", "hdcam", "hdcams", "print", "hdts", "sample", "hdtc"]:
-        buttons = [[
-            InlineKeyboardButton("10", callback_data=f"dlt#10_{query.data}")
-            ],[
-            InlineKeyboardButton("100", callback_data=f"dlt#100_{query.data}")
-            ],[
-            InlineKeyboardButton("1000", callback_data=f"dlt#1000_{query.data}")
-            ],[
-            InlineKeyboardButton('⛔️ Close', callback_data="close_data"),
-            InlineKeyboardButton('◀️ Back', callback_data="delback")
-        ]]
-        await query.message.edit(
-            text=f"<b>How Many {query.data.upper()} Files You Want To Delete?</b>",
-            reply_markup=InlineKeyboardMarkup(buttons),
-            disable_web_page_preview=True
-        )
-    
-    elif query.data.startswith("dlt#"):
-        limit, file_type = query.data.split("#")[1].split("_")
-        buttons = [[
-            InlineKeyboardButton('Hell No', callback_data=f"confirm_no")
-            ],[           
-            InlineKeyboardButton('Yes, Delete', callback_data=f"confirm_yes#{limit}_{file_type}")
-            ],[
-            InlineKeyboardButton('⛔️ Close', callback_data="close_data")
-        ]]
-        await query.message.edit(
-            text=f"<b>Are You Sure To Delete {limit} {file_type.upper()} Files?</b>",
-            reply_markup=InlineKeyboardMarkup(buttons),
-            disable_web_page_preview=True,
-        )
-    elif query.data.startswith("confirm_yes#"):
-        limits, file_type = query.data.split("#")[1].split("_")
-        limit = int(limits)
-        await delete_files(query, limit, file_type)
-
-    elif query.data == "confirm_no":
-        await query.message.edit(text=f"<b>Deletion canceled.</b>", reply_markup=None)
-
-
-    # Function for getting the top search results
-    elif query.data == "topsearch":
-
-        def is_valid_string(string):
-            return bool(re.match('^[a-zA-Z0-9 ]*$', string))
-
-        await query.answer(f"<b>Fetching Top Searches, Be Patience It'll Take Some Time</b>", show_alert=True)
-        top_searches = await mdb.get_top_messages(20)
-
-        unique_messages = set()
-        truncated_messages = []
-
-        for msg in top_searches:
-            if msg.lower() not in unique_messages and is_valid_string(msg):
-                unique_messages.add(msg.lower())
-
-                files, _, _ = await get_search_results(msg.lower())
-                if files:
-                    if len(msg) > 20:
-                        truncated_messages.append(msg[:20 - 3])
-                    else:
-                        truncated_messages.append(msg)
-
-        # Display two buttons in a row
-        keyboard = []
-        for i in range(0, len(truncated_messages), 2):
-            row_buttons = []
-            for j in range(2):
-                if i + j < len(truncated_messages):
-                    msg = truncated_messages[i + j]
-                    row_buttons.append(InlineKeyboardButton(msg, callback_data=f"search#{msg}"))
-            keyboard.append(row_buttons)
-
-        keyboard.append([
-            InlineKeyboardButton("⛔️ Close", callback_data="close_data"),
-            InlineKeyboardButton("◀️ Back", callback_data="home")
-            ])
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.message.edit(f"<b>Here is the top searches of the day</b>", reply_markup=reply_markup, disable_web_page_preview=True)
-
-    elif query.data.startswith("search#"):
-        search = query.data.split("#")[1]
-        await query.answer(text=f"Searching for your request :)")
-        premium_status = await db.is_premium_status(query.from_user.id)
-        if premium_status is True:
-            text = await callback_paid_filter(search)
-            await query.message.edit(text=f"<b>{text}</b>", disable_web_page_preview=True)
-        else:    
-            text = await callback_auto_filter(search)
-            await query.message.edit(text=f"<b>{text}</b>", disable_web_page_preview=True)
- 
-    # get download button
-    elif query.data.startswith("download#"):
-        try:
-            file_id = query.data.split("#")[1]
-            msg = await client.send_cached_media(
-                chat_id=BIN_CHANNEL,
-                file_id=file_id)
-            await client.send_message(
-                text=f"<b>Requested By</b>:\n{query.from_user.mention} <code>{query.from_user.id}</code>\n<b>Link:</b>\n{URL}/watch/{msg.id}",
-                chat_id=BIN_CHANNEL,
-                disable_web_page_preview=True)
-            online = f"{URL}/watch/{msg.id}"
-            download = f"{URL}/download/{msg.id}"
-            await query.edit_message_reply_markup(
-                reply_markup=InlineKeyboardMarkup([
-                    [InlineKeyboardButton("Watch", url=online),
-                    InlineKeyboardButton("Download", url=download)
-                    ],[
-                    InlineKeyboardButton("Close", callback_data='close_data')
-                    ]]
-            ))
-        except Exception as e:
-            await query.answer(f"Error:\n{e}", show_alert=True)
 
     await query.answer('Share & Support Us♥️')
 
@@ -922,36 +735,6 @@ async def auto_filter(client, msg, spoll=False):
     # add timestamp to database for floodwait
     await db.update_timestamps(message.from_user.id, int(time.time()))
     return f"<b>{cap}\n\n{search_results_text}</b>", InlineKeyboardMarkup(btn)
-
-# callback autofilter
-async def callback_auto_filter(msg, spoll=False):
-    search=msg
-    files, _, _ = await get_search_results(search.lower(), offset=0, filter=True)
-    # Construct a text message with hyperlinks
-    search_results_text = []
-    for file in files:
-        shortlink = await get_shortlink(f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}")
-        file_link = f"🎬 [{get_size(file.file_size)} | {await replace_blacklist(file.file_name, blacklist)}]({shortlink})"
-        search_results_text.append(file_link)
-
-    search_results_text = "\n\n".join(search_results_text)
-    cap = f"Here is what i found for your query {search}"
-    return f"<b>{cap}\n\n{search_results_text}</b>"
-
-# callback autofilter
-async def callback_paid_filter(msg, spoll=False):
-    search=msg
-    files, _, _ = await get_search_results(search.lower(), offset=0, filter=True)
-    # Construct a text message with hyperlinks
-    search_results_text = []
-    for file in files:
-        shortlink = f"https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}"
-        file_link = f"🎬 [{get_size(file.file_size)} | {await replace_blacklist(file.file_name, blacklist)}]({shortlink})"
-        search_results_text.append(file_link)
-
-    search_results_text = "\n\n".join(search_results_text)
-    cap = f"Here is what i found for your query {search}"
-    return f"<b>{cap}\n\n{search_results_text}</b>"
 
 
 async def advantage_spell_chok(msg):
