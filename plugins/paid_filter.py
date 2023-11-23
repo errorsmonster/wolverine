3f6ec2cfa719f6b3fd8bb84685e0b49c5172f9c5
L E M O N <ryme.care@hotmail.com>
2023-11-23 16:15:07 +0530

    updated

--- plugins/paid_filter.py
+++ plugins/paid_filter.py
@@ -44,7 +44,7 @@ async def paid_next_page(bot, query):
         search_results_text = []
         for file in files:
             encd_id = encode_to_base64(f"{query.from_user.id}_{file.file_id}")
-            shortlink = f"https://telegram.me/{temp.U_NAME}?start=user-{encd_id}"
+            shortlink = f"https://telegram.me/{temp.U_NAME}?start=encrypt-{encd_id}"
             file_link = f"🎬 [{get_size(file.file_size)} | {await replace_blacklist(file.file_name, blacklist)}]({shortlink})"
             search_results_text.append(file_link)
 
@@ -108,7 +108,7 @@ async def paid_filter(client, msg, spoll=False):
     search_results_text = []
     for file in files:
         encd_id = encode_to_base64(f"{message.from_user.id}_{file.file_id}")
-        shortlink = f"https://telegram.me/{temp.U_NAME}?start=user-{encd_id}"
+        shortlink = f"https://telegram.me/{temp.U_NAME}?start=encrypt-{encd_id}"
         file_link = f"🎬 [{get_size(file.file_size)} | {await replace_blacklist(file.file_name, blacklist)}]({shortlink})"
         search_results_text.append(file_link)
 
