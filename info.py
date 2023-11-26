2953a829fd17cf831551607444111066d37d3f49
L E M O N <ryme.care@hotmail.com>
2023-11-26 15:08:14 +0530

    updated

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL', '-1001596389161')
-auth_grp = environ.get('AUTH_GROUP', "-4050655009")
+auth_grp = environ.get('AUTH_GROUP', "-4068148533")
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
