9a923c83ded4aab92ff8a7e0f483edcbd5f8e0ff
L E M O N <ryme.care@hotmail.com>
2023-11-26 23:39:55 +0530

    updated

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -16,7 +16,7 @@ from pyrogram import Client, filters, enums
 from database.users_chats_db import db
 from database.config_panel import mdb
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
-from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, replace_blacklist, fetch_quote_content, save_group_settings
+from utils import get_size, is_subscribed, search_gagala, temp, get_settings, replace_blacklist, fetch_quote_content, save_group_settings
 from plugins.shortner import gplinks
 from plugins.paid_filter import paid_filter
 from plugins.free_filter import free_filter
@@ -420,11 +420,6 @@ async def advantage_spell_chok(msg):
     gs_parsed = list(dict.fromkeys(gs_parsed))  # removing duplicates https://stackoverflow.com/a/7961425
     if len(gs_parsed) > 3:
         gs_parsed = gs_parsed[:3]
-    if gs_parsed:
-        for mov in gs_parsed:
-            imdb_s = await get_poster(mov.strip(), bulk=True)  # searching each keyword in imdb
-            if imdb_s:
-                movielist += [movie.get('title') for movie in imdb_s]
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
