e458f14bd160da4ca5ed53328cfe82a8d1276c61
L E M O N <ryme.care@hotmail.com>
2023-11-27 14:07:09 +0530

    UPDATED

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -181,7 +181,7 @@ async def filters_private_handlers(client, message):
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def public_group_filter(client, message):
-
+    
     group_filter = await mdb.get_configuration_value("group_filter")
     if message.text.startswith("/") or group_filter is False:
         return
@@ -192,7 +192,7 @@ async def public_group_filter(client, message):
 
 
     await mdb.update_top_messages(message.from_user.id, message.text) 
-    
+
     group_id = message.chat.id
     member_count = message.chat.members_count
     
