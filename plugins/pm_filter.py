1f13dddc98954d2de988dc8f526748b6ef439515
L E M O N <ryme.care@hotmail.com>
2023-11-27 14:48:27 +0530

    updated

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -761,14 +761,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         title = files.file_name
         size = get_size(files.file_size)
         f_caption = files.caption
-        if CUSTOM_FILE_CAPTION:
-            try:
-                f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
-                                                       file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
-            except Exception as e:
-                logger.exception(e)
-                f_caption = f_caption
         if f_caption is None:
             f_caption = f"{title}"
         await query.answer()
