65977185647469942a83b322b428b7ab1044a654
L E M O N <ryme.care@hotmail.com>
2023-12-01 15:37:17 +0530

    updated

--- plugins/free_filter.py
+++ plugins/free_filter.py
@@ -89,7 +89,7 @@ async def free_next_page(bot, query):
 
 
 
-async def free_filter(client, msg, spoll=False):
+async def free_filter(_, msg, spoll=False):
     if not spoll:
         message = msg
         if message.text.startswith("/"): return  # ignore commands
