cb6952185a49a740ddf5f099a79d11df65bcff5a
L E M O N <ryme.care@hotmail.com>
2023-12-13 12:35:13 +0530

    updated

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -161,7 +161,6 @@ async def filters_private_handlers(client, message):
         if filter:
             await filter.delete()
 
-
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def public_group_filter(client, message):
 
@@ -174,6 +173,7 @@ async def public_group_filter(client, message):
     premium = await db.is_premium_status(message.from_user.id)
     await mdb.update_top_messages(message.from_user.id, message.text)
     
+    filter = None
     try:
         if premium:
             text, button = await paid_filter(client, message)
