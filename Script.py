2a5586747157a4c0ed8476d2d0b5f06fa251f834
L E M O N <ryme.care@hotmail.com>
2023-12-16 17:34:08 +0530

    updated

--- Script.py
+++ Script.py
@@ -60,7 +60,7 @@ By using our service, you confirm that you have read, understood, and agreed to 
     BLACKLIST = ['tamilblaster', 'filmyzilla', 'streamershub', 'xyz', 'cine', 'www', 'http', 'https',
                 'cloudsmoviesstore', 'moviez2you', 'bkp', 'cinema', 'filmy', 'flix', 'cutemoviez',
                 '4u', 'hub', 'movies', 'otthd', 'telegram', 'hoichoihok', '@', ']', '[', '📁', 'missqueenbotx',
-                'filmy', 'films', 'cinema', 'join', 'club', 'apd', 'F-Press', 'GDTOT', '♻️',
+                'filmy', 'films', 'cinema', 'join', 'club', 'apd', 'F-Press', 'GDTOT', '♻️', '\n\n'
                 'backup', 'primeroom', 'theprofffesorr', 'premium', 'vip', '4wap', 'toonworld4all', 'mlwbd',
                 'Telegram@alpacinodump', 'bollywood', "AllNewEnglishMovie", "7MovieRulz", "1TamilMV",
                 'Bazar', '_Corner20', 'CornersOfficial', 'support', 'iMediaShare', 'Uᴘʟᴏᴀᴅᴇᴅ', 'Bʏ', 'PFM', 'alpacinodump'
