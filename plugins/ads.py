a9553d3646a7278457be7494f3d5aca1596dd20d
L E M O N <ryme.care@hotmail.com>
2023-12-25 12:34:46 +0530

    updated

--- plugins/ads.py
+++ plugins/ads.py
@@ -13,7 +13,7 @@ async def set_ads(client, message):
         return
 
     ads_name, duration_or_impression = command_args.split('#', 1)
-    ads_name = ads_name.strip()
+    ads_name = f"{ads_name.strip()}"
 
     expiry_date = None
     impression_count = None
@@ -22,23 +22,23 @@ async def set_ads(client, message):
         # It's a duration
         duration = duration_or_impression[1:]
         if not duration.isdigit():
-            await message.reply_text("Duration must be a number.")
+            await message.reply_text(f"Duration must be a number.")
             return
         expiry_date = datetime.now() + timedelta(days=int(duration))
     elif duration_or_impression[0] == 'i':
         # It's an impression count
         impression = duration_or_impression[1:]
         if not impression.isdigit():
-            await message.reply_text("Impression count must be a number.")
+            await message.reply_text(f"Impression count must be a number.")
             return
         impression_count = int(impression)
     else:
-        await message.reply_text("Invalid prefix. Use 'd' for duration and 'i' for impression count.")
+        await message.reply_text(f"Invalid prefix. Use 'd' for duration and 'i' for impression count.")
         return
 
     reply = message.reply_to_message
     if not reply:
-        await message.reply_text("Reply to a message to set it as your advertisement.")
+        await message.reply_text(f"Reply to a message to set it as your advertisement.")
         return
 
     await mdb.update_advirtisment(reply.text, ads_name, expiry_date, impression_count)
